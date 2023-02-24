2592f9de3c7445df73126741152cdb3deb539b40
capyloper <farrelamarisipaenen@gmail.com>
2023-02-24 08:01:24 +0700

    initial commit

--- js/script.js
+++ js/script.js
@@ -6,3 +6,37 @@ document.querySelector("#hamburger-menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
 
+// Click outside sidebar
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener('click', function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// Change color on scroll
+const nav = document.querySelector("nav");
+const header = document.querySelector("header");
+
+const options = {
+  rootMargin: "-12%",
+};
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      nav.classList.remove("changeColor");
+    } else {
+      nav.classList.add("changeColor");
+    }
+  });
+}, options);
+
+observer.observe(header);
+
+// Carousel
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".carousel");
+  var instances = M.Carousel.init(elems, options);
+});
